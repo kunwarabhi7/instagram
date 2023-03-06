0ba4b6b314ffc38c245913c6b0c4669add3c5a32
Abhishek <abhishekparmar162@gmail.com>
2023-03-06 17:35:05 +0530

    added side portion of header

---
+++ components/Header.jsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import React from "react";
+import ig from "../public/images/ig.png";
+import {FiInstagram} from 'react-icons/fi'
+import {BsSearch  } from 'react-icons/bs'
+import {AiFillHome} from 'react-icons/ai'
+import {VscDiffAdded} from 'react-icons/vsc'
+import {RiSendPlaneFill} from 'react-icons/ri'
+import {FaRegCompass} from 'react-icons/fa'
+import {BiUser} from 'react-icons/bi'
+
+
+
+const Header = () => {
+  return (
+    <div className="">
+      <div className="flex items-center justify-around" > 
+        <div className="relative hidden w-24 h-24 cursor-pointer lg:inline-grid">
+          <Image src={ig} fill objectFit="contain" />
+        </div>
+        <div className="relative w-10 h-10 cursor-pointer lg:hidden">
+          <FiInstagram size={40} />
+        </div>
+        {/* Middle SearchBar */} 
+        <div className="max-w-xs">
+          <div className="relative p-3 mt-1 rounded-md">
+            <div className="absolute inset-y-0 flex items-center pl-3 pointer-events-none">
+              <BsSearch className="w-5 h-5 text-gray-400" />
+            </div>
+            <input
+              type="text"
+              placeholder="Search"
+              className="block w-full pl-10 placeholder-gray-400 border-gray-300 rounded-md bg-yellow-50 sm:text-sm focus:bg-white"
+            />
+          </div>
+        </div>
+        {/* Right */}
+        <div className="flex items-center justify-evenly space-x-4 cursor-pointer">
+          <AiFillHome size={25}/>
+          <RiSendPlaneFill size={25}/>
+          <VscDiffAdded size={25}/>
+          <FaRegCompass size={25}/>
+          <BiUser size={25}/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ components/Login.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <div className='w-[350px] border-2 m-16 h-[401px]'>
+<h1 className='w-[175px] h-13 text-center mt-12 ml-20 text-3xl '>Instagram</h1>
+<div className='my-4 space-y-4 text-center'>
+
+<input  placeholder='Phone number,username or email address'/>
+<input placeholder='Password'/>
+<button>Log in</button>
+</div>
+      </div>
+
+      
+    </div>
+  )
+}
+
+export default Login
