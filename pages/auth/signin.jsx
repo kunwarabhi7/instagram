39b96949fa82a3ac601eefb5800bf1f7d95bf1c6
Abhishek <abhishekparmar162@gmail.com>
2023-03-27 15:04:14 +0530

    get posts

--- pages/auth/signin.jsx
+++ pages/auth/signin.jsx
@@ -1,15 +1,15 @@
 import { getProviders, signIn } from "next-auth/react"
-import Header from '../../components/Header'
+
 import ig from '../../public/images/ig.png'
 
 const signin = ({providers}) => {
   return (
     <>
-    <Header />
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-56 px-14 text-center">
+
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-40 px-14 text-center">
       <img src={ig} alt="" />
 <p className="text-4xl mt-8 font-bold italic">Build by Abhishek</p>
-   <div className="mt-52">
+   <div className="mt-60">
     {Object.values(providers).map((provider) => (
       <div key={provider.name}>
         <button className="p-3 text-center  bg-cyan-700 text-white rounded-xl" onClick={() => signIn(provider.id ,{callbackUrl:'/'}) }>
