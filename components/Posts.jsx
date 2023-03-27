abbdf23513be906ac92c59efa91f469d5ad68c12
Abhishek <abhishekparmar162@gmail.com>
2023-03-27 14:51:53 +0530

    get posts

--- components/Posts.jsx
+++ components/Posts.jsx
@@ -1,5 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Post from "./Post";
+import {
+  collection,
+  doc,
+  getDocs,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
+import { db } from "../utils/firebase";
+import { COOKIE_NAME_PRERENDER_BYPASS } from "next/dist/server/api-utils";
+
 const DummyData = [
   {
     id: "123",
@@ -17,16 +28,30 @@ const DummyData = [
   },
 ];
 function Posts() {
+  const [posts, setPosts] = useState([]);
+  useEffect(
+    () =>
+      ( onSnapshot(
+        query(collection(db, "posts"), orderBy("timestamp", "desc")),
+        (snapshot) => {
+          setPosts(snapshot.docs)
+        }
+      )),
+
+    [db]
+  );
+
+  console.log(posts);
   return (
     <div className="">
-      {DummyData.map(( post ) => (
+      {posts.map((post) => (
         <Post
           key={post.id}
           id={post.id}
-          username={post.username}
-          userImg={post.userImg}
-          caption={post.caption}
-          img={post.img}
+          username={post.data().username}
+          userImg={post.data().profileImg}
+          caption={post.data().caption}
+          img={post.data().image}
         />
       ))}
     </div>
